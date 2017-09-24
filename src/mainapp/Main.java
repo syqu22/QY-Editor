0183c6fce473e884c1e583cc9aabc67c874f56b4
Syqu <alekkrecik@gmail.com>
2017-09-25 00:15:08 +0200

    Controller update

---
+++ src/mainapp/Main.java
@@ -0,0 +1,42 @@
+package mainapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    private long timeStart;
+    private long timeEnd;
+    private long timeElapsed;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        timeStart = System.currentTimeMillis();
+        System.out.println("QY: Application started");
+        Parent root = FXMLLoader.load(getClass().getResource("../res/mainwindow.fxml")); //startwindow.fxml
+        System.out.println("QY: FXML loaded");
+        primaryStage.setTitle("QY - Text Editor");
+        primaryStage.setMinHeight(120);
+        primaryStage.setMinWidth(300);
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+
+        System.out.println("QY: Scene showed");
+        timeEnd = System.currentTimeMillis();
+        timeElapsed = timeEnd - timeStart;
+        System.out.println();
+        System.out.println("QY: Everything loaded in " + timeElapsed + "ms");
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
