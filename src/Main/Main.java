76f9c4b549fcec0f0b4394dfe9acd9a8a5ce688b
Syqu <alekkrecik@gmail.com>
2017-09-24 23:34:24 +0200

    Small Changes + Project rebuilded

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,16 +6,29 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
+
+    private long timeStart;
+    private long timeEnd;
+    private long timeElapsed;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        System.out.println("Application started");
-        Parent root = FXMLLoader.load(getClass().getResource("../FXML/mainwindow.fxml"));
+        timeStart = System.currentTimeMillis();
+        System.out.println("QY: Application started");
+        Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml")); //startwindow.fxml
+        System.out.println("QY: FXML loaded");
         primaryStage.setTitle("QY - Text Editor");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        System.out.println("Everything is OK");
+
+        System.out.println("QY: Scene showed");
+        timeEnd = System.currentTimeMillis();
+        timeElapsed = timeEnd - timeStart;
+        System.out.println("==================================");
+        System.out.println("QY: Everything loaded in " + timeElapsed + "ms");
 
 
     }
