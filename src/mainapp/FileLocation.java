9d179943ca048c5064f87c75eb41a4882d186c40
Syqu <alekkrecik@gmail.com>
2017-09-28 22:40:02 +0200

    v.02 update

---
+++ src/mainapp/FileLocation.java
@@ -0,0 +1,16 @@
+package mainapp;
+
+class FileLocation {
+    private String location;
+
+    FileLocation(){
+    }
+
+    String getFileLocation(){
+        return location;
+    }
+
+    void setFileLocation(String location){
+         this.location = location;
+    }
+}
